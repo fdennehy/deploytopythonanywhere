0f37a06743aabb61f9b5d84c3438ebe7bfc8d869
Finbar Dennehy <finny.d@gmail.com>
2025-05-24 22:28:03 +0100

    updated DAO test script

--- account_DAO_test.py
+++ account_DAO_test.py
@@ -1,15 +1,28 @@
+# Script to test the various functions in the DAO
+
 from account_DAO import accountDAO
 
-# Get all
-print(accountDAO.getAll())
+# Get all accounts
+print("Testing all accounts function:")
+print(accountDAO.getAllAccounts())
 
-# Create new
+# Create a new account
+print("Testing account creation function:")
 new_account = {"account_name": "Test Corp", "website": "testcorp.com"}
-new_id = accountDAO.create(new_account)
-print("Created ID:", new_id)
+new_id = accountDAO.createAccount(new_account)
+print("Created account with ID:", new_id)
 
-# Update
+# Find by ID
+print("Testing find account by ID function:")
+print(accountDAO.findAccountByID(new_id))
+
+# Update the account
+print("Testing update account by ID function:")
 accountDAO.update({"id": new_id, "account_name": "Updated Corp", "website": "updatedcorp.com"})
+print("Updated account with ID:", new_id)
 
-# Delete
+# Delete the account
+print("Testing account deletion by ID function:")
 accountDAO.delete(new_id)
+print(accountDAO.findAccountByID(new_id))
+print(accountDAO.getAllAccounts())
